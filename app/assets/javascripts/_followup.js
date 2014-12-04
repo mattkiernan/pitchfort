42b4d6cedc3a815e01bc26308ef37ad9fa8c3fb2
mattkiernan <matt.kiernan@gmail.com>
2014-12-04 12:35:23 -0500

    display link WIP

--- app/assets/javascripts/_followup.js
+++ app/assets/javascripts/_followup.js
@@ -1,4 +1,9 @@
 $(function(){
+
+var accepted = $.find(".pitch-status :selected[value='Accepted']")
+var declined = $.find(".pitch-status :selected[value='Declined']")
+
+
   $("body").on("change", ".pitch-status", function(){
     var value = $(this).val();
     var followUpButton = $(this).parents().eq(3).find(".followup-button")
