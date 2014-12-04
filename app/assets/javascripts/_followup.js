749ff1172b003b3ab760d9e66d70814b1b3db083
mattkiernan <matt.kiernan@gmail.com>
2014-12-04 11:22:13 -0500

    followup button hides

---
+++ app/assets/javascripts/_followup.js
@@ -0,0 +1,9 @@
+$(function(){
+  $("body").on("change", ".pitch-status", function(){
+    var value = $(this).val();
+    var followUpButton = $(this).parents().eq(3).find(".followup-button")
+    if (value === "Accepted" || value === "Declined"){
+      followUpButton.addClass("hidden");
+    };
+  });
+});
