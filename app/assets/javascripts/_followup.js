f45a4bfed84c7a96bad6e562a65310025425204f
mattkiernan <matt.kiernan@gmail.com>
2014-12-05 12:05:45 -0500

    Extract js conditional to function

--- app/assets/javascripts/_followup.js
+++ app/assets/javascripts/_followup.js
@@ -1,20 +1,26 @@
 $(function(){
   var updateActionLink = function(select) {
     var $select = $(select);
-    var pitchState = $select.val();
+    pitchState = $select.val();
     var followupButton = $select.parents(".pitch-row").find(".followup-button");
-    if (pitchState === "Accepted" || pitchState === "Declined") {
+    if (checkIfComplete(pitchState)) {
       followupButton.hide();
     } else {
       followupButton.show();
     }
   };
 
+  var checkIfComplete = function(pitchState){
+    if (pitchState === "Accepted" || pitchState === "Declined"){
+    return true;
+    };
+  };
+
   $(".pitch-status").each(function(i, select) {
     updateActionLink(select);
   });
 
-  $("body").on("change", ".pitch-status", function() {
+  $("body").on("change", ".pitch-status", function(){
     updateActionLink(this);
   });
 });
