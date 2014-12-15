b0e94cd2b8638d8a46362858fbbd120556af67f1
mattkiernan <matt.kiernan@gmail.com>
2014-12-15 17:23:23 -0500

    Remove null contraint for oauth performance

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210033420) do
+ActiveRecord::Schema.define(version: 20141215221839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,8 +113,8 @@ ActiveRecord::Schema.define(version: 20141210033420) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "first_name",      null: false
-    t.string   "last_name",       null: false
+    t.string   "first_name"
+    t.string   "last_name"
     t.string   "provider"
     t.string   "uid"
   end
